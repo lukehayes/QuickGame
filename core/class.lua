97467cf7f803d780873413cbfce112f1a4b7898f
lukehayes <lukepurplehayes@gmail.com>
2024-03-26 10:32:56 +0000

    Added better way to do inheritance with Lua

--- core/class.lua
+++ core/class.lua
@@ -1,23 +1,40 @@
 local class = {
-    name = "classs"
+    name = "class",
+    x = 10,
+    y = 10
 }
 
-class.__index = function(k,v)
-    return class[v]
-end
+--class.__index = function(k,v)
+    --print('class __index searched')
+    --return class[v]
+--end
 
+
+function tostring(class)
+    return "Instance of " .. class
+end
 
 function class.new()
     local obj = {}
+
     setmetatable(obj, class)
+
+    -- Line below avoids the need to define class.__index = function() end ...
+    -- line commented out above.
+    class.__index = class
     return obj
 end
 
-
 function class:extends(child)
-    setmetatable( child,  { __index = self })
-    return self
-end
 
+    local t = setmetatable( child, {
+        __index = self,
+        __tostring = tostring(child.name),
+    })
+
+    --t.__index = self
+
+    return t
+end
 
 return class
