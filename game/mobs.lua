d616b649c8e577a9746901e75bcaa1adc2d9d513
lukehayes <lukepurplehayes@gmail.com>
2024-04-06 14:06:58 +0100

    Added game.mobs.lua

---
+++ game/mobs.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        name = "Basic",
+        health = 1,
+        speed = 100,
+        color = {r=1, g = 0, b = 1, a = 1}
+
+    },
+
+    {
+        name = "Goblin",
+        health = 3,
+        speed = 80,
+        color = {r=0, g = 1, b = 0, a = 1}
+    },
+
+    {
+        name = "Bat",
+        health = 2,
+        speed = 280,
+        color = {r=0.5, g = 0.2, b = 1, a = 1}
+    }
+}
