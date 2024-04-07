cd7f7afb10cf03f69493e0fc8d7dbf5ceda75a4e
lukehayes <lukepurplehayes@gmail.com>
2024-04-07 13:24:15 +0100

    Added core.math.move_towards

--- core/math.lua
+++ core/math.lua
@@ -1,9 +1,27 @@
-local math = {}
+local M = {}
 
 -- Returns the distance between two points.
-function math.distance(x1,y1, x2,y2) return ((x2-x1)^2+(y2-y1)^2)^0.5 end
+function M.distance(x1,y1, x2,y2) return ((x2-x1)^2+(y2-y1)^2)^0.5 end
 
 -- Returns the angle between two points.
-function math.angle(x1,y1, x2,y2) return math.atan2(y2-y1, x2-x1) end
+function M.angle(x1,y1, x2,y2) return math.atan2(y2-y1, x2-x1) end
 
-return math
+-- Move an entity towards a particular target.
+function M.move_towards(entity_a, entity_b, dt, threshold)
+
+    local angle = M.angle(entity_a.x, entity_a.y, entity_b.x, entity_b.y)
+    local dist  = M.distance(entity_a.x, entity_a.y, entity_b.x, entity_b.y)
+    
+    -- Distance to target threshold when movement should stop.
+    local threshold = threshold or 5
+
+    if dist > threshold then
+        entity_a.x = entity_a.x + math.cos(angle) * entity_a.speed * dt
+        entity_a.y = entity_a.y + math.sin(angle) * entity_a.speed * dt
+    else
+        entity_a.x = entity_a.x
+        entity_a.y = entity_a.y
+    end
+end
+
+return M
