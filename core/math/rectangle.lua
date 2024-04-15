9fb9c07e06c4938866ab19a9770d58a67d8115a6
lukehayes <lukepurplehayes@gmail.com>
2024-04-15 17:47:08 +0100

    Added core.math.rectangle class

---
+++ core/math/rectangle.lua
@@ -0,0 +1,39 @@
+--- Represents a generic rectangle shape with an x,y position and a width and height.
+--
+
+local Rectangle = {}
+Rectangle.__index = Rectangle
+
+function Rectangle:new(x,y,width,height)
+    local r = setmetatable({}, Rectangle)
+
+    r.x = x
+    r.y = y
+    r.width = width
+    r.height = height
+
+    return r
+end
+
+
+--- Check if another Rectangle object is completely inside this one.
+--
+-- @param Rectangle     rect    The other Rectangle to check.
+-- @return boolean    True if inside, false otherwise.
+--function Rectangle:inside(rect)
+
+    --return self.x > rect.x and
+           --self.x + self.width < rect.width + rect.x and
+           --self.y > rect.y and
+           --self.y + self.height < rect.height + rect.y
+--end
+
+function Rectangle.inside(a,b)
+
+    return a.x > b.x and
+           a.x + a.width < b.width + b.x and
+           a.y > b.y and
+           a.y + a.height < b.height + b.y
+end
+
+return Rectangle
