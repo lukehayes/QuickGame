b28dea6e3122f170be307aa919dba13d09285a90
lukehayes <lukepurplehayes@gmail.com>
2024-04-15 11:51:06 +0100

    Added many entities to test QuadTree

--- main.lua
+++ main.lua
@@ -1,41 +1,29 @@
 local Factory = require('core.object-factory')
 local R       = require('core.render')
 local P       = require('core.player')
-local V2      = require('core.math.vec2')
-local CR      = require('core.col.collision-rect')
+local QT      = require('core.col.quadtree')
 
-mm = Factory:initMegaMob(10,10)
-
+mm = Factory:initEntity(10,10)
 R:add(mm)
 
 local p = player_init(100,100, 10)
 R:add(p)
 
-local a = CR:new(300,400, 100)
-local b = CR:new(420,200, 200)
+
+for i=1,10 do
+    local rx = love.math.random(10,700)
+    local ry = love.math.random(10,600)
+    e = Factory:initMob(rx,ry)
+    R:add(e)
+end
 
 function love.load()
 end
 
 function love.update(dt)
     player_update(p, dt)
-
-    a.position.x = p.position.x
-    a.position.y = p.position.y
-
-    if a:overlaps(b) then
-        print("Overlap")
-    end
-
-    if a:inside(b) then
-        print("Inside")
-    end
-
 end
 
 function love.draw()
     R:draw_batch()
-
-    love.graphics.rectangle('line', a.position.x, a.position.y, a.scale, a.scale)
-    love.graphics.rectangle('line', b.position.x, b.position.y, b.scale, b.scale)
 end
