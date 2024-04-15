6f17d7e9b64c589849de3bf9df6d771e5b91bda4
lukehayes <lukepurplehayes@gmail.com>
2024-04-15 17:48:17 +0100

    Closer to finishing QuadTree implementation.

--- core/col/quadtree.lua
+++ core/col/quadtree.lua
@@ -1,62 +1,82 @@
-local Node = require('core.col.node')
-
+local Rectangle = require('core.math.rectangle')
 
 local QuadTree = {}
+QuadTree.__index = QuadTree
 
-function QuadTree:new(width, height)
-    local qt = {}
+function QuadTree:new(boundary)
+    local qt = setmetatable({}, QuadTree)
 
-    qt.children = {}
-    qt.max_children = 4
-    qt.width  = width
-    qt.height = height
+    qt.boundary = boundary
+    qt.points   = {}
+    qt.capacity = 4
+
+    self.ne = nil
+    self.nw = nil
+    self.se = nil
+    self.sw = nil
 
     return qt
 end
 
+function QuadTree:insert(point)
 
-function QuadTree.build_qt(qt, depth, level)
+    local size = #self.points
 
-    if level < depth then
-        qt.width = qt.width / 2
-        qt.height = qt.height / 2
-
-        level = level + 1
-
-        print(qt.width, qt.height)
-
-        QuadTree.build_qt(qt, depth, level)
+    if not Rectangle.inside(point, self.boundary) then
+        return
     end
-end
 
-function QuadTree.insert(qt)
-
-    local length = #qt.children
-
-    if length < qt.max_elems then
-        local node = Node:new(qt.width, qt.height)
-        table.insert(qt.children, node)
+    if size < self.capacity then
+        table.insert(self.points, point)
     else
-        print("Subdivide")
-        QuadTree.insert(qt)
+        self:subdivide()
+
+        self.ne:insert(point)
+        self.nw:insert(point)
+        self.se:insert(point)
+        self.sw:insert(point)
     end
 end
 
-function QuadTree.draw(qt, xp, yp)
+function QuadTree:subdivide()
 
-    -- @TODO Implmenent drawing the quad tree.
+    print("Subdividing...")
 
-    local x = 0
-    local y = 0
+    local boundary = self.boundary
 
-    --while qt.children do
-        --love.graphics.rectangle("line", x,y, qt.width, qt.height )
+    local ne = Rectangle:new(boundary.x, boundary.y, boundary.width / 2, boundary.height / 2)
+    local nw = Rectangle:new(boundary.x + boundary.width / 2, boundary.y, boundary.width / 2, boundary.height / 2)
 
-        --x = x + qt.width
-        --y = y + qt.height
+    local se = Rectangle:new(boundary.x, boundary.y + boundary.height / 2, boundary.width / 2, boundary.height / 2)
+    local sw = Rectangle:new(boundary.x + boundary.width / 2, boundary.y + boundary.height / 2, boundary.width / 2, boundary.height / 2)
 
-        --QuadTree.draw(qt, x, y)
-    --end
+    self.ne = QuadTree:new(ne)
+    self.nw = QuadTree:new(nw)
+    self.se = QuadTree:new(se)
+    self.sw = QuadTree:new(sw)
+
+end
+
+function QuadTree:draw()
+
+    love.graphics.rectangle('line', self.boundary.x, self.boundary.y, self.boundary.width, self.boundary.height)
+
+    if self.ne then
+        self.ne:draw()
+    end
+
+    if self.nw then
+        self.nw:draw()
+    end
+
+    if self.se then
+        self.se:draw()
+    end
+
+    if self.sw then
+        self.sw:draw()
+    end
+
 end
 
 return QuadTree
