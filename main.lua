ccd8abe2d83741cb93357a2dd654c21d746a5ec3
lukehayes <lukepurplehayes@gmail.com>
2024-04-16 17:41:33 +0100

    Added test movement code to main.

--- main.lua
+++ main.lua
@@ -15,36 +15,48 @@ local qtObj = QT.new(rect)
 
 local rects = {}
 
---for i=1,100 do
-    --local rx = love.math.random(10,1920)
-    --local ry = love.math.random(10,1080)
+local prssed = false
 
-    --local rect = Rect:new(rx,ry, 1,1)
-    --QT.insert(qtObj,rect)
-    --table.insert(rects, rect)
---end
+for i=1,100 do
+    local rx = love.math.random(10,1920)
+    local ry = love.math.random(10,1080)
+
+    local rect = Rect:new(rx,ry, 10,10)
+    QT.insert(qtObj,rect)
+    table.insert(rects, rect)
+end
 
 function love.load()
 end
 
 function love.update(dt)
     player_update(p, dt)
+
+    --print("Clear")
+    --QT.clear(qtObj)
+
+    for k,v in pairs(rects) do
+        v:update(dt)
+        --QT.insert(qtObj,v)
+        --table.insert(rects, v)
+    end
 end
 
 function love.draw()
     R:draw_batch()
 
-    --for k,rect in pairs(rects) do
-        --love.graphics.rectangle('line', rect.x, rect.y, rect.width, rect.height)
-    --end
+    for k,rect in pairs(rects) do
+        love.graphics.rectangle('line', rect.x, rect.y, rect.width, rect.height)
+    end
 
     QT.draw(qtObj)
 
 end
 
 function love.mousepressed(x, y, button, istouch)
-    local mx,my = love.mouse.getPosition()
-    local rect = Rect:new(mx,my, 1,1)
-    QT.insert(qtObj,rect)
-    table.insert(rects, rect)
+    --local mx,my = love.mouse.getPosition()
+    --local rect = Rect:new(mx,my, 1,1)
+    --QT.insert(qtObj,rect)
+    --table.insert(rects, rect)
+    QT.clear(qtObj)
 end
