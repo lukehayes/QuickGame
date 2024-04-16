54c0e1eecdee661f672165d2317c18f350d6bfa7
lukehayes <lukepurplehayes@gmail.com>
2024-04-16 17:41:13 +0100

    Added quick movement code to Rectangle for testing.

--- core/math/rectangle.lua
+++ core/math/rectangle.lua
@@ -12,7 +12,36 @@ function Rectangle:new(x,y,width,height)
     r.width  = width
     r.height = height
 
+    r.dx = love.math.random(-1,1)
+    r.dy = love.math.random(-1,1)
+    
+    if r.dx == 0 then
+        r.dx = love.math.random(-1,1)
+    end
+
+    if r.dy == 0 then
+        r.dy = love.math.random(-1,1)
+    end
+
+    r.speed = love.math.random(10,60)
+    
     return r
+end
+
+function Rectangle:update(dt)
+
+    self.x = self.x + self.dx + self.speed * dt
+    self.y = self.y + self.dy + self.speed * dt
+
+    if self.x < 0 or self.x > 1900 then
+        self.dx = -self.dx
+    end
+
+    if self.y < 0 or self.y > 1050 then
+        self.dy = -self.dy
+    end
+
+    print(self.dx, self.dy)
 end
 
 
@@ -22,10 +51,10 @@ end
 -- @return boolean    True if inside, false otherwise.
 function Rectangle:inside(rect)
 
-    return self.x > rect.x and
-           self.x + self.width < rect.width + rect.x and
-           self.y > rect.y and
-           self.y + self.height < rect.height + rect.y
+    return self.x >= rect.x and
+           self.x + self.width <= rect.width + rect.x and
+           self.y >= rect.y and
+           self.y + self.height <= rect.height + rect.y
 end
 
 
@@ -36,10 +65,10 @@ end
 -- @return boolean    True if inside, false otherwise.
 function Rectangle.inside(a,b)
 
-    return a.x > b.x and
-           a.x + a.width < b.width + b.x and
-           a.y > b.y and
-           a.y + a.height < b.height + b.y
+    return a.x >= b.x and
+           a.x + a.width <= b.width + b.x and
+           a.y >= b.y and
+           a.y + a.height <= b.height + b.y
 end
 
 return Rectangle
