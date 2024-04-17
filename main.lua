d98b2166b90dcdfd808d29f9343501eefb50d5d6
lukehayes <lukepurplehayes@gmail.com>
2024-04-17 17:52:03 +0100

    Updated main with latest test code.

--- main.lua
+++ main.lua
@@ -1,46 +1,45 @@
 local Factory = require('core.object-factory')
 local R       = require('core.render')
 local P       = require('core.player')
-local Rect    = require('core.math.rectangle')
-local QT      = require('core.ds.quadtree')
 local Stack   = require('core.ds.stack')
 local State   = require('core.state.state')
-local Util    = require('core.util')
+local Game    = require('core.game')
+local Rock    = require('game.rock')
+local Entity  = require('core.entity')
 
-st = State.new()
-
-local p = P.new(100,100)
-local rect = Rect:new(10,10,10,10)
+r  = Rock.new(100,300)
+e1 = Entity.new(400,200)
 
 local rocks = {}
 
-for i=1,10 do
-
-    local rx = love.math.random(10,1900)
-    local ry = love.math.random(10,1080)
-
-    local rock = Factory:initRock(rx,ry)
-    table.insert(rocks, rock)
+for i=1,100 do
+    local rx = love.math.random(-300, Game.width)
+    local ry = love.math.random(-300, Game.height)
+    local r  = Rock.new(rx,ry)
+    table.insert(rocks, r)
 end
 
+
 function love.load()
+    p = P.new(100,100)
 end
 
 function love.update(dt)
     P.update(p, dt)
 
-    print("State: ", st:current())
+    for _, rock in pairs(rocks) do
+        Rock.update(rock, dt)
+    end
 end
 
 function love.draw()
-    R:draw_batch()
-
+    --R:draw_batch()
     R:draw_sprite(p)
     --R:draw_sprite(rock)
 
-    for _,r in pairs(rocks) do
-        print(r.position)
-        R:draw_sprite(r)
+
+    for _, rock in pairs(rocks) do
+        R:draw_sprite(rock)
     end
 end
 
