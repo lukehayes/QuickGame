04969b1134895b5da0ac829c8342c3fd97e3d971
lukehayes <lukepurplehayes@gmail.com>
2024-04-19 10:14:51 +0100

    Updated tilemap code.

--- core/map/tiledmap.lua
+++ core/map/tiledmap.lua
@@ -4,14 +4,24 @@ TiledMap.__index = TiledMap
 function TiledMap.new(map, tile_image)
     local obj = setmetatable({}, TiledMap)
 
-    obj.map = require(map)
-    obj.tiles = love.graphics.newImage(tile_image)
-    obj.width = obj.map.width
-    obj.height = obj.map.height
+    obj.map        = require(map)
+    obj.texture    = love.graphics.newImage(tile_image)
+    obj.width      = obj.map.width
+    obj.height     = obj.map.height
+    obj.tilewidth  = obj.map.tilewidth
+    obj.tileheight = obj.map.tileheight
+    obj.scale = 1
+
+    --obj.quads = {
+        --love.graphics.newQuad(0, 0, obj.tilewidth, obj.tileheight, obj.scale, obj.scale),
+        --love.graphics.newQuad(16,0, obj.tilewidth, obj.tileheight, obj.scale, obj.scale),
+        --love.graphics.newQuad(32,0, obj.tilewidth, obj.tileheight, obj.scale, obj.scale)
+    --}
+
     obj.quads = {
-        love.graphics.newQuad(0, 0, 16,16, 1,1),
-        love.graphics.newQuad(16,0, 16,16, 1,1),
-        love.graphics.newQuad(32,0, 16,16, 1,1)
+        love.graphics.newQuad(0,  0, obj.tilewidth, obj.tileheight, obj.texture),
+        love.graphics.newQuad(16, 0, obj.tilewidth, obj.tileheight, obj.texture),
+        love.graphics.newQuad(32, 0, obj.tilewidth, obj.tileheight, obj.texture)
     }
 
     return obj
@@ -20,18 +30,22 @@ end
 function TiledMap:draw()
     local tilePositions = self.map.layers[1].data
 
-    print("-------")
-    local y = 0
+    local y = 1
+
     for  x = 1, self.width * self.height do
-        if x % self.width == 0 then
+
+        local quad = self.quads[tilePositions[x]]
+        local tile = tilePositions[x]
+
+        if x % 16 == 0 then
             y = y + 1
-            local tile = tilePositions[x]
-            print("Tile", tile, "X",x)
-
-            --love.graphics.draw(self.tiles, self.quads[1], x,y)
         end
-        
 
+        if quad then
+            love.graphics.draw(self.texture, self.quads[tile], x * 16, y)
+        else
+            x = x + 16
+        end
     end
 
     y = 0
--- main.lua
+++ main.lua
@@ -1,16 +1,14 @@
-local R       = require('core.render')
-local P       = require('core.player')
-local TM     = require('core.map.tiledmap')
-local Game    = require('core.game')
-local M       = require('core.math.math')
-local V2       = require('core.math.vec2')
+local R     = require('core.render')
+local P     = require('core.player')
+local TM    = require('core.map.tiledmap')
+local Game  = require('core.game')
+local M     = require('core.math.math')
+local V2    = require('core.math.vec2')
 
 map = TM.new('assets/maps/map', 'assets/maps/tiles.png')
 
 function love.load()
     p = P.new(100,100)
-
-
 end
 
 function love.update(dt)
@@ -22,8 +20,8 @@ function love.draw()
     --R:draw_batch()
     R:draw_sprite(p)
 
+    --love.graphics.setBackgroundColor(1,1,1,1)
     map:draw()
-
 end
 
 function love.mousepressed(x, y, button, istouch)
