55b07a4101f7308aeb2bc835981a6d7f5a2130a3
lukehayes <lukepurplehayes@gmail.com>
2024-04-19 17:35:31 +0100

    Added start of LuaDoc comments

--- core/color.lua
+++ core/color.lua
@@ -1,10 +1,26 @@
+--- Color Tables.
+-- Predefined colors for making it easier to add colors, mostly for testing.
+--
+-- @module Color
+
 local Color = {}
 
+--- Color.RED
 Color.RED   = {r = 1, g = 0, b = 0, a = 1}
+
+--- Color.GREEN
 Color.GREEN = {r = 0, g = 1, b = 0, a = 1}
+
+--- Color.BLUE
 Color.BLUE  = {r = 0, g = 0, b = 1, a = 1}
+
+--- Color.BLACK
 Color.BLACK = {r = 0, g = 0, b = 0, a = 1}
+
+--- Color.WHITE
 Color.WHITE = {r = 1, g = 1, b = 1, a = 1}
+
+--- Color.DEBUG
 Color.DEBUG = {r = 1, g = 0, b = 1, a = 1}
 
 return Color
