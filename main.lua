61817f2c056718d393bc7eae9078778d20a524c3
lukehayes <lukepurplehayes@gmail.com>
2024-04-24 10:03:42 +0100

    Added test camera to main

--- main.lua
+++ main.lua
@@ -4,24 +4,44 @@ local TM    = require('core.map.tiledmap')
 local Game  = require('core.game')
 local M     = require('core.math.math')
 local V2    = require('core.math.vec2')
-local STI   = require('libs.sti')
+local Camera   = require('core.camera.camera')
+local Entity = require('core.entity')
 
 map = TM.new('assets/maps/map', 'assets/maps/tiles.png')
 
+cam = Camera.new(300,100, 800,600)
+c = 0
+
+local e1 = Entity.new(10,10)
+R:add(e1)
+
 function love.load()
     p = P.new(100,100)
+    cam:setTarget(p)
 end
 
 function love.update(dt)
     P.update(p, dt)
+    c = c + 0.000001
+
+    cam:update(dt)
 end
 
 function love.draw()
-    --R:draw_batch()
-    --R:draw_sprite(p)
 
     love.graphics.setBackgroundColor(0.5,0.5,0.5,1)
+
+    cam:begin()
+
+    -- GUI BEFORE CAMERA!
+
+    --R:draw_batch()
+    cam:draw()
+    R:draw_sprite(p)
+
     map:draw()
+
+    cam:flush()
 end
 
 function love.mousepressed(x, y, button, istouch)
