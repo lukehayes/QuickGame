5a7314152e25aff9298648fe32183a841056551c
lukehayes <lukepurplehayes@gmail.com>
2024-04-28 19:02:35 +0100

    Moved object to core namespace.

---
+++ core/object.lua
@@ -0,0 +1,29 @@
+-----------------------------------------------------------
+-- Object.
+-- Base class for al things.
+--
+-- @classmod core.object
+--
+local V2 = require('core.math.vec2')
+
+local Object = {}
+Object.__index = Object
+
+-----------------------------------------------------------
+-- Constructor.
+--
+-- Create a new Object.
+--
+-- @treturn Object A new instance of Object..
+function Object.new(x,y)
+
+    Object = {
+        name  = "Object",
+        position = V2:new(x,y),
+        scale = 5,
+        color = {r=0, g = 0, b = 1, a = 1},
+        style = 'fill'
+    }
+
+    return Object
+end
