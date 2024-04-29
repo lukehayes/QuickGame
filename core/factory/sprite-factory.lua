201b88c4675af1b7f9585be88e99fb1755aff929
lukehayes <lukepurplehayes@gmail.com>
2024-04-29 17:15:37 +0100

    Updated SpriteFactory path for pico_man

--- core/factory/sprite-factory.lua
+++ core/factory/sprite-factory.lua
@@ -6,7 +6,7 @@ SpriteFactory.spaceship:setFilter('nearest', 'nearest')
 SpriteFactory.spaceship_blur = love.graphics.newImage('assets/spaceship-blur.png')
 SpriteFactory.spaceship:setFilter('nearest', 'nearest')
 
-SpriteFactory.pico_man = love.graphics.newImage('assets/Pico8-Man.png')
+SpriteFactory.pico_man = love.graphics.newImage('assets/images/Pico8-Man.png')
 SpriteFactory.pico_man:setFilter('nearest', 'nearest')
 
 return SpriteFactory
