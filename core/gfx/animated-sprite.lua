58cd897d6feede26f4487bfd712288745fdcbc29
lukehayes <lukepurplehayes@gmail.com>
2024-04-30 17:39:30 +0100

    Added basic implemention of an animated sprite.

--- core/gfx/animated-sprite.lua
+++ core/gfx/animated-sprite.lua
@@ -4,7 +4,8 @@
 --
 -- @classmod core.gfx.animated-sprite
 --
-local Entity  = require('core.entity.entity')
+local Sprite  = require('core.gfx.sprite')
+local JSON    = require('libs.json')
 
 local AnimatedSprite = {}
 AnimatedSprite.__index = AnimatedSprite
@@ -15,12 +16,55 @@ AnimatedSprite.__index = AnimatedSprite
 -- Create a new AnimatedSprite.
 --
 -- @treturn AnimatedSprite A new instance of AnimatedSprite..
-function AnimatedSprite.new(x,y)
+function AnimatedSprite.new(x,y,image, speed)
 
-    local obj = Entity.new(x,y)
+    local obj = Sprite.new(x,y, image)
     setmetatable(obj, AnimatedSprite)
+    setmetatable(AnimatedSprite, Sprite)
 
-    return AnimatedSprite
+    local fh = io.open('assets/images/Pico8-Man.json')
+    obj.frame_data = JSON.decode(fh:read("*all"))
+    fh:close()
+
+    obj.frame       = 5
+    obj.max_frame   = 9
+    obj.frame_timer = 0
+    obj.tile_size   = 8
+    obj.playing     = false
+
+    -- Generate all of the quads for the tilemap.
+    obj.quads = {}
+    for i=1,obj.image:getWidth() / obj.tile_size do
+
+        local x = i * obj.tile_size - obj.tile_size
+        local quad = love.graphics.newQuad(x,0, obj.tile_size, obj.tile_size, obj.image)
+        table.insert(obj.quads, quad)
+    end
+
+    obj.anim_speed = 0.2
+
+    return obj
+end
+
+function AnimatedSprite:update(dt)
+
+    if self.playing then
+        self.frame_timer = self.frame_timer + dt
+
+        if self.frame_timer > self.anim_speed then
+            self.frame = self.frame + 1
+            self.frame_timer = 0
+        end
+
+        if self.frame >= self.max_frame then
+            self.frame = 5
+        end
+    end
+
+end
+
+function AnimatedSprite:draw()
+    love.graphics.draw(self.image, self.quads[self.frame],  self.position.x, self.position.y, 0, self.scale, self.scale)
 end
 
 return AnimatedSprite
