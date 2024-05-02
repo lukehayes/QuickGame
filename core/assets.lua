977c23bc8d1b82af88696c7f765a1d9cf86a8773
lukehayes <lukepurplehayes@gmail.com>
2024-05-02 12:12:39 +0100

    Added Assets.get_json()

--- core/assets.lua
+++ core/assets.lua
@@ -11,5 +11,15 @@ function Assets.get_image(image)
     return 'assets/images/' .. image .. '.png'
 end
 
+---------------------------------------------------------------------------------
+-- Get the path to a particular image.
+--
+-- @tparam string json    The name of the json file.
+--
+-- @treturn String    The full path to the json file, nil otherwise.
+function Assets.get_json(json)
+    return 'assets/json/' .. json .. '.json'
+end
+
 
 return Assets
