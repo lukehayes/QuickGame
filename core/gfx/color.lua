2654ed23864da2476a674549848c66b212799614
lukehayes <lukepurplehayes@gmail.com>
2024-05-03 17:31:41 +0100

    Added LIGHT_GRAY and DARK_GRAY colors.

--- core/gfx/color.lua
+++ core/gfx/color.lua
@@ -6,11 +6,14 @@
 -- @module core.gfx.color
 local Color = {}
 
---- Color.RED
--- @table Color.Red
+-----------------------------------------------------------
+-- Color.RED
+-- @table Color.RED
 Color.RED   = {r = 1, g = 0, b = 0, a = 1}
 
---- Color.GREEN
+-----------------------------------------------------------
+-- Color.GREEN
+-- @table Color.GREEN
 Color.GREEN = {r = 0, g = 1, b = 0, a = 1}
 
 --- Color.BLUE
@@ -21,6 +24,12 @@ Color.BLACK = {r = 0, g = 0, b = 0, a = 1}
 
 --- Color.WHITE
 Color.WHITE = {r = 1, g = 1, b = 1, a = 1}
+
+--- Color.LIGHT_GRAY
+Color.LIGHT_GRAY = {r = 0.8, g = 0.8, b = 0.8, a = 1}
+
+--- Color.DARK_GRAY
+Color.DARK_GRAY = {r = 0.25, g = 0.25, b = 0.25, a = 1}
 
 --- Color.DEBUG
 Color.DEBUG = {r = 1, g = 0, b = 1, a = 1}
