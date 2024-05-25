8af2ff4148782162c7b10c584977635b5f563c34
lukehayes <lukepurplehayes@gmail.com>
2024-05-25 09:29:40 +0100

    Updated main

--- main.lua
+++ main.lua
@@ -1,31 +1,19 @@
 local R        = require('core.gfx.render')
 local Color   = require('core.gfx.color')
 local Entity  = require('core.entity.entity')
-local StateEntity  = require('core.entity.state_entity')
-
---local StateEntity   = require('core.entity.state_entity')
---local as1 = AnimSprite.new(100,100, Assets.image('Pico8-Man'))
---local down = false
---local toggled = false
---local stateEntity = StateEntity.new(300,300, Assets.image('Pico8-Man'))
---as1 = stateEntity
-
+local AnimSprite  = require('core.gfx.animated-sprite')
 
 local e1 = Entity.new(300,300)
-e1.color = {r=0.1, g = 0.8, b = 0, a = 1}
+e1.color = {r=1, g = 0.8, b = 1, a = 1}
 e1.speed = 300
 
 local e2 = Entity.new(100,500)
+e2.color = {r=1, g = 0.8, b = 1, a = 1}
 
 local e3 = Entity.new(600,500)
 
-local s1 = StateEntity.new(100,100, 'assets/images/Pico8-Man.png', 0.2, 'Idle')
-
-print(e1.name)
-print(e2.name)
-print(e1.position)
-print(e2.position)
-
+local s1 = AnimSprite.new(100,100, 'assets/images/Pico8-Man.png', 0.2, 'Idle')
+s1.scale = 10
 
 function love.load()
 end
@@ -33,18 +21,19 @@ end
 function love.update(dt)
     e1:update(dt)
     e2:update(dt)
+
+    s1:update(dt)
 end
 
 function love.draw()
 
     R:clear(Color.BLACK)
 
-    R:draw(e1)
-    R:draw(e2)
-    R:draw(e3)
+    --R:draw(e1)
+    --R:draw(e2)
+    --R:draw(e3)
 
-    R:draw_sprite(s1)
-
+    s1:draw()
 end
 
 function love.mousepressed(x, y, button, istouch)
