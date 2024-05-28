a01e13ff0cdf0a4f8b6b4753977ea242eaf317f0
lukehayes <lukepurplehayes@gmail.com>
2024-05-28 09:52:12 +0100

    Started rewrite of ECS

--- game/ecs/ecs.lua
+++ game/ecs/ecs.lua
@@ -2,4 +2,37 @@ ECS = {}
 ECS.__index = ECS
 
 
+function ECS.new()
+    local obj = setmetatable({}, ECS)
+
+    obj.components = {
+        transform = {}
+    }
+
+    return obj
+end
+
+function ECS:find_entity(id)
+    --TODO Find all of the components that have a specific ID.
+end
+
+function ECS:sort_entities(id)
+    --TODO Sort all of the entities to be in ID order.
+end
+
+function ECS:add_transform(x, y, id)
+
+    local transform = {
+        x     = x,
+        y     = y,
+        w     = 10,
+        h     = 10,
+        id    = id,
+        color = {r=255, g = 0, b = 0, a = 0}
+    }
+
+    table.insert(self.components.transform, transform)
+end
+
+
 return ECS
--- game/ecs/system/render.lua
+++ game/ecs/system/render.lua
@@ -2,31 +2,29 @@
 -- Render System
 -- ********************************************
 
-function update(entities)
-	if entities == nil then return end
+function update(components)
 
-	print(entities)
+    if components == nil then return end
 
-		love.graphics.setColor(255,255,255,255)
-	for k,v in pairs(entities) do
+    for k, v in pairs(components.transform) do
 
-		love.graphics.setColor(
-			v.color.r,
-			v.color.g,
-			v.color.b,
-			v.color.a
-		)
+        --love.graphics.setColor(
+            --v.color.r,
+            --v.color.g,
+            --v.color.b,
+            --v.color.a
+        --)
 
-		love.graphics.rectangle(
-			"fill",
-			v.transform.x,
-			v.transform.y,
-			v.transform.w,
-			v.transform.h
-		)
-		love.graphics.setColor(255,255,255,255)
-	end
-	love.graphics.setColor(255,255,255,255)
+        love.graphics.rectangle(
+            "fill",
+            v.x,
+            v.y,
+            v.w,
+            v.h
+        )
+
+        --love.graphics.setColor(255,255,255,255)
+    end
 
 end
 
