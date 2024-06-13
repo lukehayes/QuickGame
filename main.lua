e3662a36cc70d99ba31260c8906b7a9ac2256deb
lukehayes <lukepurplehayes@gmail.com>
2024-06-13 10:11:52 +0100

    Updated main

--- main.lua
+++ main.lua
@@ -6,8 +6,8 @@ local System = require('game.ecs.system.system')
 local e = ECS.new()
 
 local t = e:add_transform(1, 100,100)
-e:add_collision(1,50,50)
-e:add_sprite(1, "assets/images/face.png", 4)
+e:add_collision(1,8, 8)
+e:add_sprite(1, "assets/images/spaceship-blue.png", 4)
 
 e:add_transform(2, 300,340)
 e:add_collision(2, 2,2)
@@ -17,17 +17,18 @@ e:add_collision(3, 100,100)
 
 --e:add_sprite(2, "assets/images/face.png")
 
---for i=1,10 do
-    --local rx = love.math.random(10,1200)
-    --local ry = love.math.random(10, 720)
-    --local rc = love.math.random(1,100)
+for i=1,100 do
+    local rx = love.math.random(10,1200)
+    local ry = love.math.random(10, 720)
+    local rc = love.math.random(1,100)
 
-    --e:add_transform(i, rx,ry)
+    e:add_transform(i, rx,ry)
+    e:add_sprite(i, "assets/images/spaceship-blue.png",4)
 
-    --if rc > 50 then
-        --e:add_collision(i,32,32)
-    --end
---end
+    if rc > 50 then
+        e:add_collision(i,32,32)
+    end
+end
 
 function love.load()
 end
@@ -40,7 +41,8 @@ end
 function love.draw()
 
     R:clear(Color.BLACK)
-    System.render(e.components, true)
+    --System.render(e.components, true)
+    System.render(e.components, false)
 end
 
 function love.mousepressed(x, y, button, istouch)
