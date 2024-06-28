c1b3a8fdae1893ba9ba05cde53f4a5263c070df2
lukehayes <lukepurplehayes@gmail.com>
2024-06-28 09:44:14 +0100

    Updated main

--- main.lua
+++ main.lua
@@ -3,46 +3,32 @@ local Color  = require('core.gfx.color')
 local ECS    = require('game.ecs.ecs')
 local System = require('game.ecs.system.system')
 local e = ECS.new()
+local delta = 0
 
-local base_size = 1
+function change_color(t)
+    t.position.x = love.math.random(10,600)
+    t.position.y = love.math.random(10,600)
+    t.color.r    = love.math.random(0.2,1.0)
+    t.color.g    = love.math.random(0.2,1.0)
+    t.color.b    = love.math.random(0.2,1.0)
+end
+
+function change_size(t)
+    t.w = love.math.random(8, 96)
+    t.h = love.math.random(8, 96)
+end
 
 e:add_transform(1, 400,100)
+e:add_timer(1,1, 0.1, 10,  change_color, "change_color")
 --e:add_sprite(1, "spaceship-blue", 4)
 --e:add_collision(1, 48,48)
 --e:add_physics(1, 30, 30)
-e:add_timer(1,1, 1, 3,  function() print("Entity 1 CB 1") end)
---e:add_timer(1,2, 2,  7, function()  print("Entity 1 CB 2")  end)
 
+e:add_transform(2, 300,300)
+--e:add_timer(2,1, 1.5, 5,  change_color, "change_color")
+e:add_timer(2,1, 0.7, 100,  change_size, "change_size")
 
---
---e:add_transform(2, 200,500)
---e:add_timer(2,1, 0.3, false)
---e:add_timer(3,2, 0.3, false)
---e:add_transform(2, 300,100)
---e:add_sprite(2, "debug32", 2)
---e:add_physics(2, 30,30)
---e:add_collision(2, 64,64)
-
-local delta = 0
-
-
---for i=2,10 do
-    --local rx = love.math.random(10,1260)
-    --local ry = love.math.random(10,700)
-    --local rs = love.math.random(1,100)
-
-   --e:add_transform(i, rx,ry)
-   --if rs > 50 then
-        --e:add_sprite(i, "spaceship-red", 4)
-   --else
-        --e:add_sprite(i, "spaceship-blue", 4)
-   --end
-
-
-   --e:add_physics(i, 30,30)
-   --e:add_collision(i, 32,32)
---end
-
+e:add_transform(3, 100,500)
 
 function love.load()
 end
@@ -52,13 +38,11 @@ function love.update(dt)
     
     delta = dt
 
-    if toggle then
-        -- Collision and Physics order is important!
-        System.collision(e.components)
-        System.physics(e.components, dt)
-        System.move(e.components, dt)
-        System.timer(e.components, dt)
-    end
+    -- Collision and Physics order is important!
+    --System.collision(e.components)
+    --System.physics(e.components, dt)
+    --System.move(e.components, dt)
+    System.timer(e.components, dt)
 end
 
 function love.draw()
@@ -76,8 +60,8 @@ function love.keypressed(key, scancode, isrepeat)
       love.event.quit()
    end
 
-   if key == "space" then
-       print("Space")
-        toggle = not toggle
-   end
+   --if key == "space" then
+       --print("Space")
+        --toggle = not toggle
+   --end
 end
