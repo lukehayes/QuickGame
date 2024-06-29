6f6d7ed43b45ccc9b839aefacb90c348238d1cd2
lukehayes <lukepurplehayes@gmail.com>
2024-06-29 10:54:56 +0100

    Added player to main

--- main.lua
+++ main.lua
@@ -2,19 +2,31 @@ local R      = require('core.gfx.render')
 local Color  = require('core.gfx.color')
 local ECS    = require('core.ecs.ecs')
 local System = require('core.ecs.system.system')
+local Player = require('core.player')
 
 local delta = 0
 
+local t = 0
+
+function love.draw()
+end
+
 function love.load()
+    p = Player.new(100,100, 10)
 end
 
 function love.update(dt)
     delta = dt
+
+    Player.update(p, dt)
 end
 
 function love.draw()
 
     R:clear(Color.BLACK)
+    R:draw(p)
+    Player.show_velocity(p)
+
     -- ECS
     --System.render(e.components, true)
     --System.render(e.components, false)
